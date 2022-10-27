af4a0003b08884b0a932d0fab846389b8ec426ad
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-10-27 21:36:01 +0700

    Add scenario four

--- scenarioFour.cpp
+++ scenarioFour.cpp
@@ -1,12 +1,22 @@
-#include <chrono>
+#include <unistd.h>
 #include <iostream>
-// ...
-
+#include <cstdlib>
+#include <random>
+#include <chrono>
 using namespace std;
 using namespace chrono;
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count()); //For LL
+
 int main(){
-milliseconds ms = duration_cast< milliseconds >(
-    system_clock::now().time_since_epoch()
-);
-cout << ms.count() << endl;
+  const int OneSecond = 1e6;
+  const int HalfSecond = OneSecond>>1;
+  for(int i = 1;i <= 100;i++){
+    double sleepDuration = (rng()%OneSecond) + HalfSecond;
+    usleep(sleepDuration);
+    milliseconds ms = duration_cast< milliseconds >(
+        system_clock::now().time_since_epoch()
+    );
+    cout << ms.count() << endl;
+  }
+  return 0;
 }
