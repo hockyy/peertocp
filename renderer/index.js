47a92da03be137b986c03a2eb884e77ba15953d9
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-11-04 13:17:51 +0700

    Update Time

--- renderer/index.js
+++ renderer/index.js
@@ -622,7 +622,7 @@ const checker = () => {
     log.transports.file.resolvePath = () => `out/${logID}.log`
     log.info("Inserting test for " + currentID)
     log.info("logID is " + logID)
-    const msLeft = Date.parse("2022-11-04T13:17:00.000+07:00") - Date.now()
+    const msLeft = Date.parse("2022-11-04T13:19:00.000+07:00") - Date.now()
     // setTimeout(scenarioOne, msLeft)
     // setTimeout(() => {
     //   codemirrorView.dispatch({
